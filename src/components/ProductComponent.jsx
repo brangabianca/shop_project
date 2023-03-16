d522f6fca3c88e76f66d8df2fd1e4d4a6b820682
brangabianca <brangabianca@yahoo.com>
2023-03-16 18:01:58 +0200

    wishlist functionality & Home page Carousel

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -2,6 +2,28 @@ import { Card, CardBody, CardTitle, Button, Col } from "reactstrap";
 import "./ProductComponent.style.css";
 
 function ProductComponent({ product }) {
+	const addToWishlist = () => {
+		const productsString = localStorage.getItem("products");
+
+		if (productsString !== null) {
+			const products = JSON.parse(productsString);
+
+			const existNr = products.find((productElement) => {
+				return productElement.id === product.id;
+			});
+
+			if (existNr === undefined) {
+				products.push(product);
+			}
+
+			localStorage.setItem("products", JSON.stringify(products));
+		} else {
+			const newProducts = [];
+			newProducts.push(product);
+			localStorage.setItem("products", JSON.stringify(newProducts));
+		}
+	};
+
 	return (
 		<Col className='product_card mt-4'>
 			<Card>
@@ -10,7 +32,13 @@ function ProductComponent({ product }) {
 				<CardBody className='product_cardbody'>
 					<CardTitle tag='h5'>{product.title}</CardTitle>
 					<p>{product.price}$</p>
-					<Button className='product_button'>Add to wishlist!</Button>
+					<Button
+						onClick={() => {
+							addToWishlist();
+						}}
+						className='product_button'>
+						Add to wishlist!
+					</Button>
 				</CardBody>
 			</Card>
 		</Col>
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,4 +1,87 @@
+import { useEffect, useState } from "react";
+import { Button, Table } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./Wishlist.style.css";
+
 function Wishlist() {
-	return <h1>Wishlist Page</h1>;
+	const [wishlistProducts, setWishlistProducts] = useState([]);
+
+	useEffect(() => {
+		const productsString = localStorage.getItem("products");
+		if (productsString !== null) {
+			const products = JSON.parse(productsString);
+			setWishlistProducts(products);
+		}
+	}, []);
+
+	const onDelete = (productId) => {
+		const filteredProducts = wishlistProducts.filter(
+			(product) => product.id !== productId
+		);
+		localStorage.setItem("products", JSON.stringify(filteredProducts));
+		setWishlistProducts(filteredProducts);
+	};
+
+	return (
+		<>
+			<div className='d-flex'>
+				<h3 className='wishlist_title'>Wishlist</h3>
+				<Link className='link_home_wishlist' to='/home'>
+					Home
+				</Link>
+				<Link className='link_produse_wishlist' to='/products'>
+					/ Produse
+				</Link>
+				<img
+					className='wishlist_image_background'
+					src='https://images.unsplash.com/photo-1511025998370-7d59f82e9c8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80'
+				/>
+			</div>
+
+			<Table bordered>
+				<thead>
+					<tr>
+						<th></th>
+						<th></th>
+						<th>Product Name</th>
+						<th>Unit Price</th>
+						<th>Remove</th>
+					</tr>
+				</thead>
+				{wishlistProducts.map((product, index) => {
+					return (
+						<tbody key={"product_" + index}>
+							<tr>
+								<th></th>
+								<img className='wishlist_image' src={product.image} />
+								<td>{product.title}</td>
+								<td>{product.price}</td>
+								<td>
+									<Button
+										color='danger'
+										onClick={() => {
+											onDelete(product.id);
+										}}>
+										Delete
+									</Button>
+								</td>
+							</tr>
+						</tbody>
+					);
+				})}
+			</Table>
+
+			{/* <ul className='wishlist_container d-flex'>
+				{wishlistProducts.map((product, index) => {
+					return (
+						<div className='d-flex' key={"product_" + index}>
+							<li className='mt-2'>{product.title}</li>
+							<img className='wishlist_image' src={product.image} />
+						</div>
+					);
+				})}
+			</ul> */}
+		</>
+	);
 }
 export default Wishlist;
